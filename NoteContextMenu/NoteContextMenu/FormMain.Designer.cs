6a621fec58b995c17a1bdd2742af43600f44e78f
it2051229 <contact@it2051229.com>
2016-01-08 10:42:27 +0800

    Initial commit
    First working version.

---
+++ NoteContextMenu/NoteContextMenu/FormMain.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NoteContextMenu
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerNoteToContextMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unregisterNoteFromContextMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.cleanNotesDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxNote
+            // 
+            this.textBoxNote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxNote.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNote.Location = new System.Drawing.Point(0, 46);
+            this.textBoxNote.Multiline = true;
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.Size = new System.Drawing.Size(244, 194);
+            this.textBoxNote.TabIndex = 0;
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(244, 24);
+            this.menuStripMain.TabIndex = 2;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registerNoteToContextMenuToolStripMenuItem,
+            this.unregisterNoteFromContextMenuToolStripMenuItem,
+            this.cleanNotesDatabaseToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // registerNoteToContextMenuToolStripMenuItem
+            // 
+            this.registerNoteToContextMenuToolStripMenuItem.Name = "registerNoteToContextMenuToolStripMenuItem";
+            this.registerNoteToContextMenuToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.registerNoteToContextMenuToolStripMenuItem.Text = "&Register \'Note\' to context menu";
+            this.registerNoteToContextMenuToolStripMenuItem.Click += new System.EventHandler(this.registerNoteToContextMenuToolStripMenuItem_Click);
+            // 
+            // unregisterNoteFromContextMenuToolStripMenuItem
+            // 
+            this.unregisterNoteFromContextMenuToolStripMenuItem.Name = "unregisterNoteFromContextMenuToolStripMenuItem";
+            this.unregisterNoteFromContextMenuToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.unregisterNoteFromContextMenuToolStripMenuItem.Text = "&Unregister \'Note\' from context menu";
+            this.unregisterNoteFromContextMenuToolStripMenuItem.Click += new System.EventHandler(this.unregisterNoteFromContextMenuToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(265, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonSave.Location = new System.Drawing.Point(0, 240);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(244, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save Note";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxPath.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPath.Location = new System.Drawing.Point(0, 24);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(244, 22);
+            this.textBoxPath.TabIndex = 4;
+            // 
+            // cleanNotesDatabaseToolStripMenuItem
+            // 
+            this.cleanNotesDatabaseToolStripMenuItem.Name = "cleanNotesDatabaseToolStripMenuItem";
+            this.cleanNotesDatabaseToolStripMenuItem.Size = new System.Drawing.Size(268, 22);
+            this.cleanNotesDatabaseToolStripMenuItem.Text = "Clean \'Notes\' database";
+            this.cleanNotesDatabaseToolStripMenuItem.Click += new System.EventHandler(this.cleanNotesDatabaseToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 263);
+            this.Controls.Add(this.textBoxNote);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.menuStripMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MainMenuStrip = this.menuStripMain;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Note Context Menu";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerNoteToContextMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unregisterNoteFromContextMenuToolStripMenuItem;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleanNotesDatabaseToolStripMenuItem;
+    }
+}
+
---
+++ NoteContextMenu/NoteContextMenu/FormMain.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteContextMenu
+{
+    public partial class FormMain : Form
+    {
+        private string NOTES_FILENAME = AppDomain.CurrentDomain.BaseDirectory + "\\notes.dat";
+        private Dictionary<string, string> notes;
+
+        /// <summary>
+        /// Register for context menu
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        public FormMain(string[] args)
+        {
+            InitializeComponent();
+
+            notes = new Dictionary<string, string>();
+            loadNotes();
+
+            
+            if(args.Length > 0)
+            {
+                textBoxPath.Text = args[0];
+
+                if (notes.ContainsKey(args[0]))
+                {
+                    textBoxNote.Text = notes[args[0]];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Save all notes to file
+        /// </summary>
+        private void saveNotes()
+        {
+            Stream stream = File.Open(NOTES_FILENAME, FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, notes);
+            stream.Close();
+        }
+
+        /// <summary>
+        /// Load all notes from file
+        /// </summary>
+        private void loadNotes()
+        {
+            try
+            {
+                Stream stream = File.Open(NOTES_FILENAME, FileMode.Open);
+                BinaryFormatter formatter = new BinaryFormatter();
+                notes = (Dictionary<string, string>)formatter.Deserialize(stream);
+                stream.Close();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Unregister the context menu
+        /// </summary>
+        private void unregisterContextMenu()
+        {
+            FileShellExtension.Unregister("*", "note");
+            FileShellExtension.Unregister("Directory", "note");
+        }
+
+        /// <summary>
+        /// Save the note
+        /// </summary>
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (textBoxPath.Text == string.Empty || textBoxNote.Text.Trim() == string.Empty)
+                return;
+
+            if (notes.ContainsKey(textBoxPath.Text))
+            {
+                // Update existing note
+                notes[textBoxPath.Text] = textBoxNote.Text.Trim();
+            }
+            else
+            {
+                // Add new note
+                notes.Add(textBoxPath.Text, textBoxNote.Text.Trim());
+            }
+
+            saveNotes();
+            Close();
+        }
+
+        /// <summary>
+        /// Add the note option to the windows context menu
+        /// </summary>
+        private void registerNoteToContextMenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // full path to self, %L is placeholder for selected file
+            string menuCommand = string.Format("\"{0}\" \"%L\"", Application.ExecutablePath);
+
+            // register the context menu to directory, files, and drives
+            FileShellExtension.Register("*", "note", "Note", menuCommand);
+            FileShellExtension.Register("Directory", "note", "Note", menuCommand);
+
+            MessageBox.Show("Ok, 'note' menu is now available to your files and folders.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Remove the note option from the windows context menu
+        /// </summary>
+        private void unregisterNoteFromContextMenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileShellExtension.Unregister("*", "note");
+            FileShellExtension.Unregister("Directory", "note");
+
+            MessageBox.Show("Ok, 'note' menu has been removed from your files and folders.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Close the program
+        /// </summary>
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Show about
+        /// </summary>
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("www.it2051229.com", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Delete all notes that are unlinked (no related files)
+        /// </summary>
+        private void cleanNotesDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach(KeyValuePair<string, string> entry in notes)
+            {
+                if(Directory.Exists(entry.Key) || File.Exists(entry.Key))
+                {
+                    // Don't delete note if directory file exists
+                    continue;
+                }
+
+                notes.Remove(entry.Key);
+            }
+
+            saveNotes();
+            MessageBox.Show("All notes which has its related files moved/removed has been deleted.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
